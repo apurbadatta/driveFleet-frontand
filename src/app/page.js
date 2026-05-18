c6a52f31ad16abf1083024edb6a298106059bd3e
apurbadatta <apurbadatta76761@gmail.com>
2026-05-19 02:30:56 +0600

    lauout pblm solve

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,13 @@
 import BannerPage from "./components/Banner";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 
 
 export default function Home() {
   return (
     <div>
-      <Navbar></Navbar>
+     
       <BannerPage></BannerPage>
       
-      <Footer></Footer>
+      
     </div>
   );
 }
